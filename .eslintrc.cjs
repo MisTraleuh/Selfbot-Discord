fe506a351ddc0854b615318d83a3e537ab3b4a52
Nathan Catalan <nathan.catalan@epitech.eu>
2022-06-08 10:33:56 +0200

    ➕ eslint convention

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: false,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+  }
+}
